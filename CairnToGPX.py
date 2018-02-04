70658c78e404bb0dd435442223788bc38bdcf903
Tyler Hunt <tyty5949@gmail.com>
2018-02-03 19:13:38 -0600

    Initial commit

---
+++ CairnToGPX.py
@@ -0,0 +1,8 @@
+'''
+
+'''
+
+if __name__ == "__main__":
+	print("Welcome to CairnMe to GPX!")
+	print("Type 'convert' to convert your Cairn to a GPX file.")
+	print("    or type 'help' for more information.")
